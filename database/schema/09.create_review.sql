35dfaa745bda06bd76ada8f740aae1d440c2b47b
azure928 <ji@DESKTOP-UBR9OBO.localdomain>
2022-06-23 16:30:25 +0900

    review 브랜치 생성

--- database/schema/09.create_review.sql
+++ database/schema/09.create_review.sql
@@ -3,7 +3,9 @@ review text NOT NULL,
 score INT,
 user_id INT NOT NULL,
 room_id INT NOT NULL,
+reservation_id INT NOT NULL,
 PRIMARY KEY(id),
 FOREIGN key(user_id) REFERENCES users(id) ON DELETE CASCADE,
-FOREIGN key(room_id) REFERENCES room(id) ON DELETE CASCADE)
+FOREIGN key(room_id) REFERENCES room(id) ON DELETE CASCADE),
+FOREIGN key(reservation_id) REFERENCES reservation(id) ON DELETE CASCADE)
 
