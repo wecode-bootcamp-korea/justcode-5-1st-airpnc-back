4959d34c5f541e864b7f1ab9b12135befc77bba9
azure928 <ji@DESKTOP-UBR9OBO.localdomain>
2022-06-27 06:03:42 +0900

    Add: getReview (리뷰 불러오는 api) - server.js에 app.get('/review/2'... 로 작성함ser - 아직 Layered Pattern 적용하지 않음

--- server.js
+++ server.js
@@ -5,14 +5,47 @@ const express = require('express');
 const cors = require('cors');
 
 const routes = require('./routes');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
+// 리뷰 불러오기 (2번 방에 대한 리뷰)
+app.get('/review/2', async (req, res) => {
+  // 1. 데이터를 가지고 온다. 2번 데이터만
+  const reviews = await prisma.$queryRaw`
+    select users.name, users.created_at, review.review
+    FROM review
+    LEFT JOIN users ON review.user_id = users.id
+    where room_id=2;
+    `;
+  console.log('어디갔어', reviews);
+
+  // 2. return response
+  return res.status(200).json({ data: reviews });
+});
+
+app.get('/ping', (req, res) => {
+  res.json({ message: 'pong' });
+});
+
 const server = http.createServer(app);
 const PORT = process.env.PORT || 10010;
-server.listen(PORT, () => {
-  console.log(`server start : http://localhost:${PORT}/`);
-});
+
+// console.log(PORT);
+
+const start = async () => {
+  // 서버를 시작하는 함수입니다.
+  try {
+    server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+  } catch (err) {
+    console.error(err);
+    await prisma.$disconnect(); // 에러가 발생했을 시에 database 연결을 종료합니다.
+  }
+};
+
+start();
