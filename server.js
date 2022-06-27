2598fb1d6a1ea665a105ab48be219c2b0c56d3dd
김민욱 <minuk3508@gmail.com>
2022-06-27 21:32:54 +0900

    add: 로그인 인증기능 추가/토큰부여 아직

--- server.js
+++ server.js
@@ -11,6 +11,32 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
+const users = {
+  email: ['minuk3508@gmail.com'],
+  password: ['1234567'],
+};
+
+app.post('/users/login', (req, res) => {
+  console.log(req.body);
+  const email = req.body.email;
+  const password = req.body.password;
+  if (users.email.includes(email)) {
+    const emailIndex = users.email.indexOf(email);
+    if (users.password[emailIndex] === password) {
+      return res.json({
+        success: true,
+      });
+    }
+  }
+  return res.json({
+    success: false,
+    msg: '로그인에 실패하였습니다.',
+  });
+});
+app.get('/ping', (req, res) => {
+  res.json({ message: 'pong' });
+});
+
 const server = http.createServer(app);
 const PORT = process.env.PORT || 10010;
 server.listen(PORT, () => {
