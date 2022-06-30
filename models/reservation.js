32d5274fe23c7c218825346a8dc564bdf99175ea
KIMYECHAN <kyc5244@gmail.com>
2022-06-30 17:53:57 +0900

    MODIFY : reservation sql문 수정

--- models/reservation.js
+++ models/reservation.js
@@ -13,22 +13,21 @@ async function makeReservation(reservationDto) {
 async function getReservationList(user_id) {
   const ReserevationList =
     await prisma.$queryRaw`SELECT reservation.id, reservation.room_id, reservation.reservation_no, 
-  reservation.created_at, reservation.guests, reservation.check_in, reservation.check_out, photo.file_url,
+  reservation.created_at, reservation.guests , reservation.check_in, reservation.check_out, (SELECT JSON_ARRAYAGG(CASE WHEN photo.file_url IS NOT NULL THEN JSON_OBJECT('url',photo.file_url) END) FROM photo GROUP BY room_id) AS photo_url,
    room.name,city.name AS city,city.country from reservation JOIN room ON reservation.room_id = room.id JOIN room_city ON reservation.room_id = room_city.room_id 
-   JOIN city ON room_city.city_id=city.id JOIN photo ON reservation.room_id = photo.room_id WHERE reservation.user_id = ${user_id}`;
+   JOIN city ON room_city.city_id=city.id WHERE reservation.user_id = ${user_id}`;
   return ReserevationList;
 }
 
 async function getToReviewList(user_id) {
   const ToReviewList =
-    await prisma.$queryRaw`SELECT ANY_VALUE(reservation.id) AS reservation_id , ANY_VALUE(reservation.room_id) AS room_id, 
-    room.name,ANY_VALUE(city.name) AS city,ANY_VALUE(city.country) AS country, ANY_VALUE(review.review)  AS review, JSON_ARRAYAGG(CASE WHEN photo.file_url IS NOT NULL THEN JSON_OBJECT('url',photo.file_url) END) AS photo_url
+    await prisma.$queryRaw`SELECT reservation.id,reservation.room_id, 
+    room.name,city.name AS city, city.country , review.review, (SELECT JSON_ARRAYAGG(CASE WHEN photo.file_url IS NOT NULL THEN JSON_OBJECT('url',photo.file_url) END) FROM photo GROUP BY room_id) AS photo_url
      from reservation JOIN room ON reservation.room_id = room.id 
     JOIN room_city ON reservation.room_id = room_city.room_id 
-   JOIN city ON room_city.city_id=city.id JOIN photo ON reservation.room_id = photo.room_id
+   JOIN city ON room_city.city_id=city.id
    LEFT JOIN review ON reservation.id = review.reservation_id 
-   WHERE reservation.user_id = ${user_id} AND reservation.check_out <= NOW() AND review.review is null 
-   GROUP BY room.id`;
+   WHERE reservation.user_id = ${user_id} AND reservation.check_out <= NOW() AND review.review is null `;
   return ToReviewList;
 }
 
